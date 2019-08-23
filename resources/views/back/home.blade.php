a29f4597b70bea71ceb4e17e1d714340855d9044
Nicolas Disant <n.disant@hotmail.com>
2019-08-23 16:44:08 +0200

    statistique admin v3

--- resources/views/back/home.blade.php
+++ resources/views/back/home.blade.php
@@ -1,33 +1,38 @@
 @extends('layouts.back')
 
 @section('content')
-    <div id="pieChartsElements"></div>
+    <div id="pieChartsElements" class="row"></div>
 @endsection
 
 
 @section('scripts')
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+<script src="{{asset('js/charts.js')}}"></script>
 <script>
+document.addEventListener("DOMContentLoaded", function() {
     let datas = [];
 
     datas.push(@json($datas_q6));
     datas.push(@json($datas_q7));
     datas.push(@json($datas_q8));
 
+    let colors = ['red', 'blue', 'green', 'yellow', 'black', 'brown'];
+
     for (let i = 0; i < datas.length; i++) {
+        let datasLabel = [];
         let datasValue = [];
-        let labels = [];
         Object.keys(datas[i]).map(key => {
-            labels.push(key);
+            datasLabel.push(key);
         });
         Object.values(datas[i]).map(value => {
             datasValue.push(value);
         });
 
+        let div = document.createElement('div');
+        div.setAttribute('class', 'col-md-6');
         let canvas = document.createElement('canvas');
         canvas.id = 'data_' + i;
-        document.getElementById('pieChartsElements').appendChild(canvas);
+
+        document.getElementById('pieChartsElements').appendChild(div).appendChild(canvas);
 
         let ctx = document.getElementById('data_' + i).getContext('2d');
         let pieChart = new Chart(ctx, {
@@ -35,12 +40,13 @@
             data: {
                 datasets: [{
                     data: datasValue,
+                    backgroundColor: colors
                 }],
-                labels: labels,
+                labels: datasLabel,
             },
         });
-
     }
+});
 </script>
         
 @endsection
